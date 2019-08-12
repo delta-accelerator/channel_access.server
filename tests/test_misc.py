b39cce440ea6798024735e8b4395bc086204038d
André Althaus <andre.althaus@tu-dortmund.de>
2019-08-12 16:07:44 +0200

    Don't enforce a fixed array size
    The array size depends on the length of the value attribute and
    is not fixed after creating the PV.

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -15,3 +15,18 @@ def test_alias(server):
     server.removeAlias('CAS:Alias')
     with pytest.raises(common.CagetError):
         common.caget('CAS:Alias')
+
+def test_dynamic_size(server):
+    pv = server.createPV('CAS:Test', ca.Type.CHAR, attributes = {
+        'value': 42
+    })
+    assert(not pv.is_array)
+    assert(pv.count == 1)
+    value = int(common.caget('CAS:Test'))
+
+    pv.value = [1, 2, 3]
+    assert(pv.is_array)
+    assert(pv.count == 3)
+    value = list(map(int, common.caget('CAS:Test', array=True)))
+    assert(len(value) == 3)
+
